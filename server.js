7a9953aecc2ecf94242e96b48a883ab72113c9d9
Qiyuan Chen <qiyuan.chen2002@gmail.com>
2022-03-02 02:52:21 +0000

    version 1.1 fix 1

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.listen(port, () => {
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("web/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "build", "index.html"));
+	});
 }
 
 app.use(express.urlencoded({ extended: true }));
